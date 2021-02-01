3c39d9e01a243b400a67bb51ce58a4cbb9bd00c9
Anna Chang <p227738930@hotmail.com>
2021-02-01 16:46:48 +0800

    Sticker

--- app.py
+++ app.py
@@ -51,7 +51,10 @@ def handle_message(event):
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=r))
+        StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+    )
 
 
 if __name__ == "__main__":
