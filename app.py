63ae17fd83bf80286006167b6f14a31a48f410b1
Anna Chang <p227738930@hotmail.com>
2021-02-01 16:16:02 +0800

    update

--- app.py
+++ app.py
@@ -38,10 +38,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = '你吃飯了嗎'
+    r = '我看不懂~~~'
+    
+    if msg == 'hi':
+        r = 'hi'
+    elif msg == '你吃飯了嗎':
+        r = 'Not yet'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=s))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
